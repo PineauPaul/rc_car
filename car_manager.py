fe64e19b3fead39cdf869f9b9e299fa63431c41a
PineauPaul <paul.pineau@ensta-bretagne.org>
2024-07-10 16:19:26 +0200

    udpate code

--- car_manager.py
+++ car_manager.py
@@ -2,6 +2,7 @@ from motor import Motor
 from servo import Servo
 from face_detect import FaceDetector
 
+
 class CarManager():
     def __init__(self):
         self.state = [0,0,0] #X,Y,theta
@@ -9,6 +10,18 @@ class CarManager():
         self.servo = Servo()
         self.motor = Motor()
 
-    
+    def get_face_target(self):
+        resolution = self.face_detector.get_resolution()
+        for (x, y, w, h) in self.face_detector.detect_face():
+            errx = (resolution[0]/2) - x
+            erry = (resolution[1]/2) - y
+
+            dist = (w*h)/(resolution[0]*resolution[1])
+
+        return errx,erry,dist
+
+    # def regulate(self):
+
+
 if __name__=="__main__":
     print('Run car manager')
--- face_detect.py
+++ face_detect.py
@@ -8,6 +8,7 @@ class FaceDetector():
         self.camera_source = camera_source
         self.setting_file = setting_file
         self.plot = False
+        self.resolution = (640,480)
 
     def __del__(self):
         self.video_capture.release()
@@ -17,13 +18,18 @@ class FaceDetector():
     def camera_init(self):
         self.faceCascade = cv2.CascadeClassifier(self.setting_file)
         self.video_capture = cv2.VideoCapture(self.camera_source)
+        print("Width = ", self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print("Height = ", self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.resolution = (self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH),self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def get_resolution(self):
+        return self.resolution
 
     def set_plot(self, value):
         self.plot = value
 
     def detect_face(self):
         ret, frame = self.video_capture.read()
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = self.faceCascade.detectMultiScale(
